cf49e137743e4559646afb42b4d3ba54ffa566e3
DavidWuthier <dwuthier@gmail.com>
2021-06-23 13:14:30 +0200

    Færdig videre

--- uas_trajectory.m
+++ uas_trajectory.m
@@ -6,18 +6,19 @@
 
 % Trajectory generation
 % start_point = [double(0), double(-2), double(0)]
-% temp_point_1 = p_h1-(u_h1/10)
-% temp_point_2 = p_h1+(u_h1/10)
+temp_point_1 = p_h1-(u_h1/10)
+temp_point_2 = p_h1+(u_h1/10)
 
 
 
-knots = [0 5 10];
-waypoints = cell(1,3);
-waypoints{1} = [0;-2;2]
+knots = [0 2];
+waypoints = cell(1,2);
+waypoints{1} = [0;-2;0]
+waypoints{2} = [0;-2;2]
 
-waypoints{2} = [0.43;-0.10;1.31]
+% waypoints{3} = [temp_point_1(1); temp_point_1(2); temp_point_1(3)];
 %waypoints{2} = [0 ; -2 ; 3];
-waypoints{3} = [0.23;-0.1;1.3]
+% waypoints{4} = [temp_point_2(1); temp_point_2(2); temp_point_2(3)];
 % Fix this...
 order = 7;
 corridors.times = [1];
